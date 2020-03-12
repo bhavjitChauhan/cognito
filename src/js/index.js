58393ef99df3667811635ed0072e676b90dbac02
Bhavjit Chauhan <bhavjitchauhan@gmail.com>
2020-03-12 16:38:46 +0000

    Add basic results for Wikipedia API

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,18 @@
 console.log("Hello World!");
 
-const form = document.querySelector('#search-form');
+const searchBox = document.querySelector('#search');
 
-form.addEventListener('submit', search);
+searchBox.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    search(event);
+  }
+});
 
 function search(event) {
-    event.preventDefault();
-
     const input = document.querySelector('#search').value;
     const query = input.trim();
 
@@ -16,7 +22,11 @@ function search(event) {
 function fetchWikipedia(query) {
     const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${query}`;
     $.get(endpoint, data => {
-        console.log(data);
+        console.log(data)
+        let title = `<b>${data.query.search[0].title}</b>`;
+        // let link = `<a href=\"${}\"><i class=\"fas fa-external-link-square-alt\"></i></a>`
+        let snippet = `<p>${data.query.search[0].snippet}</p>`;
+        $("#results").append(`<div>${title + snippet}</div>`);
     })
         .fail(() => {
             console.error("Wikipedia Failed");
